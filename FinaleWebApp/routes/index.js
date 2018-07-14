2cdae42da212bf0f046fd726382a88bd84a4880e
Nauka Moors <nauka.moors@informatik.hs-fulda.de>
2018-07-14 19:56:41 +0200

    Add random Ghibli Movie

--- FinaleWebApp/routes/index.js
+++ FinaleWebApp/routes/index.js
@@ -4,17 +4,20 @@ var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
- 
-request('https://ghibliapi.herokuapp.com/films', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
- // console.log('body:', body); // Print the HTML for the Google homepage.
-  obj = JSON.parse(body);
-  console.log(obj.value.title);
-});
+  
+  request('https://ghibliapi.herokuapp.com/films', function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // console.log('body:', body); // Print the HTML for the Google homepage.
+    obj = JSON.parse(body);
+    // erg = JSON.stringify(obj[i].title);
+    //console.log(obj.length);
+  const i = Math.floor(Math.random()*20);
+  console.log(obj[i].title);
+  erg = JSON.stringify(obj[i].title);
+ });
 
-  res.render('index', { title: 'Express' });
-
+  res.render('index', { title: 'Nandas Blog', ghibli: erg});
 
 });
 
