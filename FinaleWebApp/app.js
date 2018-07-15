eae54c2aa95c19e2c2fc39ae6105438989d61664
Nauka Moors <nauka.moors@informatik.hs-fulda.de>
2018-07-15 17:33:50 +0200

    del HTMl & routes

--- FinaleWebApp/app.js
+++ FinaleWebApp/app.js
@@ -38,8 +38,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-router.get("/main", (req, res) => {
-  res.render("main.pug");
-});
+
 
 module.exports = app;
--- FinaleWebApp/routes/users.js
+++ FinaleWebApp/routes/users.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/main', function(req, res, next) {
+  console.log(test);
+  res.send('main.pug', {title: 'Nandas Blog'});
+});
+
+
 module.exports = router;
